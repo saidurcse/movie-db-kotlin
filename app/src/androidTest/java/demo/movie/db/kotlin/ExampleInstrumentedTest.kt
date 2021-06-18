e99ab740c386690cb5f6e6ec165d835298c95c55
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-06-18 18:14:00 +0600

    First commit.

---
+++ app/src/androidTest/java/demo/movie/db/kotlin/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package demo.movie.db.kotlin
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("demo.movie.db.kotlin", appContext.packageName)
+    }
+}
