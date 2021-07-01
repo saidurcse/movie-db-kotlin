c45979dcc5f50dda3fd1159d418fc1d87a755625
Saidur Rahman <saidur.rahman@bistasolutions.com>
2021-07-01 20:59:55 +0600

    Email validation junit code added.

--- app/src/main/java/demo/movie/db/kotlin/database/MyAppDataBase.kt
+++ app/src/main/java/demo/movie/db/kotlin/database/MyAppDataBase.kt
@@ -6,7 +6,7 @@ import androidx.room.TypeConverters
 import demo.movie.db.kotlin.database.dao.MovieLocalDataDAO
 import demo.movie.db.kotlin.ui.home.model.Movie
 
-@Database(entities = [Movie::class], version = 2, exportSchema = false)
+@Database(entities = [Movie::class], version = 1, exportSchema = false)
 @TypeConverters(DataConverter::class)
 abstract class MyAppDataBase : RoomDatabase() {
     abstract fun movieLocalDataDAO(): MovieLocalDataDAO?
--- app/src/main/java/demo/movie/db/kotlin/ui/login/LoginFragment.kt
+++ app/src/main/java/demo/movie/db/kotlin/ui/login/LoginFragment.kt
@@ -9,6 +9,7 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import demo.movie.db.kotlin.R
 import demo.movie.db.kotlin.databinding.FragmentMoviedbLoginBinding
+import demo.movie.db.kotlin.utils.EmailValidator
 import demo.movie.db.kotlin.utils.SharedPreferencesHelper
 import demo.movie.db.kotlin.utils.SharedPreferencesKey.FIRST_TIME
 import demo.movie.db.kotlin.utils.SharedPreferencesKey.LOGIN_USEREMAIL
@@ -16,11 +17,14 @@ import demo.movie.db.kotlin.utils.SharedPreferencesKey.LOGIN_USEREMAIL
 class LoginFragment : Fragment(), View.OnClickListener {
     private lateinit var bindingView: FragmentMoviedbLoginBinding
     private lateinit var sharedPreferences: SharedPreferencesHelper
+    private var mEmailValidator: EmailValidator? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
         bindingView = FragmentMoviedbLoginBinding.inflate(layoutInflater, container, false)
         sharedPreferences = SharedPreferencesHelper(requireContext())
+        mEmailValidator = EmailValidator()
+        bindingView.inputEmailId.addTextChangedListener(mEmailValidator)
 
         if(sharedPreferences.get(FIRST_TIME, false)!!) {
             findNavController().navigate(R.id.movie_home)
@@ -35,6 +39,10 @@ class LoginFragment : Fragment(), View.OnClickListener {
     override fun onClick(clickedView: View) {
         when(clickedView.id) {
             R.id.login_button -> {
+                if(!mEmailValidator!!.isValid){
+                    bindingView.inputEmailId.error = "Invalid email"
+                    return
+                }
                 val user_email = bindingView.inputEmailId.text.toString()
                 val password = bindingView.inputPassword.text.toString()
 
